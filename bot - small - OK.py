913445d4c79e477860c815ac84b5e3af6a134a8f
leaverpool <44435104+leaverpool@users.noreply.github.com>
2018-10-24 18:21:12 +0300

    123
    123

---
+++ bot - small - OK.py
@@ -0,0 +1,10 @@
+import requests
+from time import sleep
+
+while True:
+    g = requests.get('https://api.telegram.org/bot652844002:AAFPHFs48zVNiEoNv9Yp1rpp4l2fmBjOZ20/getUpdates')
+    g = requests.get('https://api.telegram.org/bot652844002:AAFPHFs48zVNiEoNv9Yp1rpp4l2fmBjOZ20/sendMessage?chat_id=292397556&text=test')
+    print(g)
+    print(g.text)
+    sleep(100)
+
--- bot.py
+++ bot.py
@@ -1,10 +1,35 @@
 import requests
 from time import sleep
 
-while True:
-    g = requests.get('https://api.telegram.org/bot652844002:AAFPHFs48zVNiEoNv9Yp1rpp4l2fmBjOZ20/getUpdates')
-    g = requests.get('https://api.telegram.org/bot652844002:AAFPHFs48zVNiEoNv9Yp1rpp4l2fmBjOZ20/sendMessage?chat_id=292397556&text=test')
-    print(g)
-    print(g.text)
-    sleep(100)
+url = "https://api.telegram.org/bot652844002:AAFPHFs48zVNiEoNv9Yp1rpp4l2fmBjOZ20/"
 
+
+def get_updates_json(request):
+    response = requests.get(request + 'getUpdates')
+    return response.json()
+
+
+def last_update(data):
+    results = data['result']
+    total_updates = len(results) - 1
+    return results[total_updates]
+
+def get_chat_id(update):
+    chat_id = update['message']['chat']['id']
+    return chat_id
+
+def send_mess(chat, text):
+    params = {'chat_id': chat, 'text': text}
+    response = requests.post(url + 'sendMessage', data=params)
+    return response
+
+def main():
+    update_id = last_update(get_updates_json(url))['update_id']
+    while True:
+        if update_id == last_update(get_updates_json(url))['update_id']:
+           send_mess(get_chat_id(last_update(get_updates_json(url))), 'test1')
+           update_id += 1
+        sleep(10)
+
+if __name__ == '__main__':
+    main()
