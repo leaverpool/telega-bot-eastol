8d00dde6d1bd3dd4d090df16709e52cfb0e3df35
leaverpool <44435104+leaverpool@users.noreply.github.com>
2018-10-24 18:03:22 +0300

    asd
    asd

--- bot.py
+++ bot.py
@@ -1,26 +1,10 @@
 import requests
+from time import sleep
 
-url = "https://api.telegram.org/bot652844002:AAFPHFs48zVNiEoNv9Yp1rpp4l2fmBjOZ20/"
+while True:
+    g = requests.get('https://api.telegram.org/bot652844002:AAFPHFs48zVNiEoNv9Yp1rpp4l2fmBjOZ20/getUpdates')
+    g = requests.get('https://api.telegram.org/bot652844002:AAFPHFs48zVNiEoNv9Yp1rpp4l2fmBjOZ20/sendMessage?chat_id=292397556&text=test')
+    print(g)
+    print(g.text)
+    sleep(100)
 
-
-def get_updates_json(request):
-    response = requests.get(request + 'getUpdates')
-    return response.json()
-
-
-def last_update(data):
-    results = data['result']
-    total_updates = len(results) - 1
-    return results[total_updates]
-
-def get_chat_id(update):
-    chat_id = update['message']['chat']['id']
-    return chat_id
-
-def send_mess(chat, text):
-    params = {'chat_id': chat, 'text': text}
-    response = requests.post(url + 'sendMessage', data=params)
-    return response
-
-chat_id = get_chat_id(last_update(get_updates_json(url)))
-send_mess(chat_id, 'Your message goes here')
